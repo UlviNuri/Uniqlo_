8e9897b70e8a0a95bf41185f0997f7bad58af375
UlviNuri <102859099+UlviNuri@users.noreply.github.com>
2022-07-07 18:59:15 +0400

    new

---
+++ assest/js/app.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const Multiplied = document.getElementById("multiplied");
+const Decreasing = document.getElementById("decreasing");
+const Value = document.getElementById("coxalan");
+
+Multiplied.addEventListener("click", MultipliedFunction);
+Decreasing.addEventListener("click", DecreasingFunction);
+
+
+
+var sum=0;
+
+function MultipliedFunction(e) {
+  e.preventDefault();
+  sum++;
+  sum=inn
+}
+
+function DecreasingFunction(e) {
+  e.preventDefault();
+  console.log("ccmso");
+}
